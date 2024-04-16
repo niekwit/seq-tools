252603da3be424763b7f946af9ce15e03fdb6344
niekwit <41302859+niekwit@users.noreply.github.com>
2024-04-16 15:22:05 +0100

    removed pandas dependency

--- md5sums.py
+++ md5sums.py
@@ -6,7 +6,6 @@ import os
 import hashlib
 import glob
 import csv
-import pandas as pd
 
 def md5(file):
     """
@@ -26,28 +25,30 @@ if len(md5sum_files) == 0:
     if len(md5sum_files) == 0:
         raise ValueError("No md5sum files found with extensions .md5 or .md5sums.txt...")
     
-# Read in the md5sum files
-files = []
-md5sums_txt = []
-md5sums_calculated = []
-for md5sum_file in md5sum_files:
-    with open(md5sum_file, "r") as csvfile:
-        reader = csv.reader(csvfile, delimiter=' ')
-        for row in reader:
-            row = [x for x in row if x != ""]
-            print(f"Checking md5sum for {row[1]}...")
-            files.append(row[1])
-            md5sums_txt.append(row[0])
-            md5sums_calculated.append(md5(row[1]))
+# Read in the md5sum files and check the md5sums
+with open("md5sums.csv", "w") as outfile:
+    writer = csv.writer(outfile, delimiter=',')
+    
+    # Write header to outfile
+    writer.writerow(["file", 
+                     "md5sum_txt", 
+                     "md5sum_calculated", 
+                     "md5sum_match"])
+    
+    for md5sum_file in md5sum_files:
+        with open(md5sum_file, "r") as csvfile:
+            reader = csv.reader(csvfile, delimiter=' ')
             
-# Create a pandas dataframe
-print("Writing results to md5sums.csv...")
-df = pd.DataFrame(data={"file": files, 
-                        "md5sum_txt": md5sums_txt, 
-                        "md5sum_calculated": md5sums_calculated})
+            for row in reader:
+                # Tidy up row
+                row = [x for x in row if x != ""]
+                print(f"Checking md5sum for {row[1]}...")
+                md5sums_txt=row[0]                
+                file = row[1]
+                md5sums_calculated = md5(row[1])
 
-df["md5sum_match"] = df["md5sum_txt"] == df["md5sum_calculated"]
-
-# Write df to file
-df.to_csv("md5sums.csv", index=False)
-print("Done!")
+                # Write to outfile
+                writer.writerow([file, 
+                                 md5sums_txt, 
+                                 md5sums_calculated, 
+                                 md5sums_txt == md5sums_calculated])
