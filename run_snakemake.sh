ea25460ccdf57ea07c8b32968e13d4e562739ff6
niekwit <41302859+niekwit@users.noreply.github.com>
2024-04-25 09:23:52 +0100

    Initial commit

---
+++ run_snakemake.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+## Run this script from directory with config, reads and workflow folders present ##
+
+# Check if mamba is installed
+CHECK=$(which mamba)
+if [ "$CHECK" == "" ]; then
+    echo "mamba not found..."
+    echo "Please install mamba using 'conda install -c conda-forge mamba'"
+    exit 1
+fi
+
+# Check if snakemake8 mamba env is available
+CHECK=$(mamba env list | grep snakemake8 | awk '{print $1}')
+if [ "$CHECK" != "snakemake8" ]; then
+    echo "snakemake8 mamba env not found..."
+    echo "Creating snakemake8 mamba env..."
+    mamba create -c conda-forge -c bioconda -n snakemake8 snakemake
+else
+    echo "Activating snakemake8 mamba env..."
+    mamba activate snakemake8
+fi
+
+# Create rule graph
+mkdir -p images
+snakemake --forceall --rulegraph | grep -v '\-> 0\|0\[label = \"all\"' | dot -Tpng > images/rule_graph.png
+
+# Dry run
+echo "Running dry run..."
+snakemake -np > /dev/null
+if $? != 0; then
+    echo "Dry run failed..."
+    snakemake -np
+    exit 1
+fi
+
+# Run workflow
+PROFILE="/home/niek/.config/snakemake/standard/"
+snakemake --profile $PROFILE
+
+# Create report
+snakemake --report report.html
+
