c16ea867e87630e2babd37cc06ca19da6cd05f61
niekwit <41302859+niekwit@users.noreply.github.com>
2024-05-01 08:38:51 +0100

    create directories if in name

--- rename.py
+++ rename.py
@@ -1,5 +1,6 @@
 import csv
 import os
+from pathlib import Path
 
 # Open the CSV file
 with open('rename.csv', 'r') as file:
@@ -10,6 +11,9 @@ with open('rename.csv', 'r') as file:
         # Get the current and new file names from the row
         current_name = f"reads/{row[0]}"
         new_name = f"reads/{row[1]}"
+        
+        if "/" in row[1]:
+            Path(os.path.dirname(new_name)).mkdir(parents=True, exist_ok=True)
         
         # Rename the file
         os.rename(current_name, new_name)
